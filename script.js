64e85db45101cd9c4851e52deef4494a5d32a03e
Evan Ramirez <159038154+eRAM50@users.noreply.github.com>
2025-12-11 19:25:54 -0600

    Add files via upload
    Added button change light/dark when clicked

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function toggleTheme() { 
     const body = document.body;
+    const btn = document.getElementById("mode-btn");
     body.classList.toggle("light-mode");
+    if (body.classList.contains("light-mode")) {
+        btn.textContent = "🌙 Dark Mode";
+    } else {
+        btn.textContent = "🌓 Light Mode";
+    }
 }
